b4d88622dba174abb1be35d855a04485ece97a03
Aviad Sapir <Aviad@Renews-MacBook-Pro.local>
2019-04-28 00:37:11 -0400

    Updates

---
+++ precache-manifest.c90d39c3e9a3aa3226b5448a76ea76c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f78906a733342f2d383",
+    "url": "/connect-four/static/js/runtime~main.000e00dd.js"
+  },
+  {
+    "revision": "0d5c545284b5cc9e42a3",
+    "url": "/connect-four/static/js/main.f61149ce.chunk.js"
+  },
+  {
+    "revision": "e0503efc3264c42068e3",
+    "url": "/connect-four/static/js/2.bd5183c1.chunk.js"
+  },
+  {
+    "revision": "0d5c545284b5cc9e42a3",
+    "url": "/connect-four/static/css/main.1d79fbd3.chunk.css"
+  },
+  {
+    "revision": "28f70255b874eb82dd1facbc163bacdb",
+    "url": "/connect-four/index.html"
+  }
+];
